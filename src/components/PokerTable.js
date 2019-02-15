365fc0d98ccf65190b1d5a35d21e033f82cdecf5
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-15 15:30:12 -0500

    starting gamebuttons

--- src/components/PokerTable.js
+++ src/components/PokerTable.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Deck from "../utilityClasses/Deck";
+import GameButtons from "./GameButtons";
 
 class PokerTable extends Component{
     constructor(){
@@ -9,13 +10,28 @@ class PokerTable extends Component{
         this.cards.shuffleDeck();
         console.log(this.cards.deck)
         this.state = {
-
+            playersHand: [],
+            dealersHand: []
         }
     }
+    // this is a custom method that isn't coming from react
+    prepDeck(){
+        const card1 = this.cards.deck.shift();
+        const card2 = this.cards.deck.shift();
+        const card3 = this.cards.deck.shift();
+        const card4 = this.cards.deck.shift();
+        this.setState({
+            playersHand: [card1,card3],
+            dealersHand: [card2,card4]
+        })
+    }
+
     render(){
         return(
             <div className="col-sm-12 the-table">
-            
+                <PokerHand cards={this.state.dealersHand}/>
+                <PokerHand cards={this.state.playersHand}/>
+                <GameButtons/>
             </div>
         )
     }
