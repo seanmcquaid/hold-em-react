88b8a29916e23cca634a1b7c36d3d18f2e434f40
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-18 10:51:27 -0500

    created bet function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PokerTable from "./components/PokerTable";
-import './App.css';
 
 class App extends Component {
   render() {
--- src/components/PokerTable.js
+++ src/components/PokerTable.js
@@ -12,9 +12,13 @@ class PokerTable extends Component{
         console.log(this.cards.deck)
         this.state = {
             playersHand: ["purple_back","purple_back"],
-            dealersHand: ["purple_back","purple_back"]
+            dealersHand: ["purple_back","purple_back"],
+            communityCards : ["purple_back","purple_back","purple_back","purple_back","purple_back"],
+            wager: 0,
+            bankRoll: 10000
         }
         this.prepDeck = this.prepDeck.bind(this)
+        this.playerBet = this.playerBet.bind(this)
     }
     // this is a custom method that isn't coming from react
     prepDeck(){
@@ -24,17 +28,52 @@ class PokerTable extends Component{
         const card4 = this.cards.deck.shift();
         this.setState({
             playersHand: [card1,card3],
-            dealersHand: [card2,card4]
+            dealersHand: [card2,card4],
+        })
+    }
+
+    // this method will be sent to gamebuttons and is used to update the player bet
+    // after they bet, we will call draw
+    playerBet(amount){
+        // this.state.wager += amount = NEVER DO THIS!!!!
+        // LET REACT CHANGE STATE
+        this.setState({
+            wager: this.state.wager + amount,
+            bankRoll: this.state.bankRoll - amount
+        })
+        this.draw();
+    }
+
+    // draw is called whenever a new community card needs to be drawn
+
+    draw(){
+        // we have to use Object.assign (or ...) to make a seperate copy of state
+        let communityNewhand = Object.assign([],this.state.communityCards)
+        console.log(communityNewhand)
+        if(communityNewhand[0] === "purple_back"){
+            // this is the first draw, so draw 3 cards
+            communityNewhand = [this.cards.deck.shift(),this.cards.deck.shift(),this.cards.deck.shift()]
+        }else{
+            // this isnt the first draw, so I will only draw one
+            communityNewhand.push([this.cards.deck.shift()]);
+        }
+        this.setState({
+            communityCards: communityNewhand
         })
     }
 
     render(){
         return(
             <div className="col-sm-12 the-table">
+                <div className="col-sm-12 text-center">
+                Current Wager : ${this.state.wager}
+                Current Bankroll : ${this.state.bankRoll}
+                </div>
                 <PokerHand cards={this.state.dealersHand}/>
+                <PokerHand cards={this.state.communityCards}/>
                 <PokerHand cards={this.state.playersHand}/>
                 {/* can pass FUNCTIONS AS A PROP IF YOU DONT CALL IT  */}
-                <GameButtons dealFunction={this.prepDeck} />
+                <GameButtons dealFunction={this.prepDeck} betFunction={this.playerBet}/>
             </div>
         )
     }
